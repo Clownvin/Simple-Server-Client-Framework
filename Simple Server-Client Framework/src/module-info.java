3852ccffa0df9b0b65bcc1488160e4a3633e5dcc
Calvin Hall <123clownvin@gmail.com>
2019-03-11 15:51:56 -0500

    Woops, forgot to change module

--- Simple Server-Client Framework/src/module-info.java
+++ Simple Server-Client Framework/src/module-info.java
@@ -7,7 +7,7 @@
  */
 module com.git.clownvin.simpleserverframework {
 	requires java.desktop;
-	requires com.git.clownvin.util;
+	requires com.clownvin.util;
 	exports com.git.clownvin.simplescframework;
 	exports com.git.clownvin.simplescframework.connection;
 }
